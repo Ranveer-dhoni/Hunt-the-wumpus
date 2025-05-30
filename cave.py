51d10b86d44d8cdc43778a1094eabee85ec02af2
Ranveer-dhoni <ranveer.rattan@education.nsw.gov.au>
2025-05-30 00:42:54 +0000

    Created and added more features to the cave.py and main.py

---
+++ cave.py
@@ -0,0 +1,19 @@
+class Cave: 
+    def_init_(self, cave_name):
+        self.name = cave_name
+        self.description = None 
+
+    def set_name(self, set_name)
+        self.name = cave_name 
+
+        def get_name(self):
+            return self.name 
+
+    def set_definition(self, cave_description)
+        self.description = cave_description
+
+    def get_decription(self):
+        return self.description 
+     
+    def describe(self):
+        print(self.descrpition)
---
+++ main.py
@@ -0,0 +1,6 @@
+ from cave import Cave 
+
+ cavern = Cave("Cavern")
+ cavern.set_definition("A damp and diry cave")
+ grotto = Cave("Grotto")
+ dungeon = Cave("Dungeon")
