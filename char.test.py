4825ab79e9564fd1e686a0110e28cf3660a8402c
Ranveer-dhoni <ranveer.rattan@education.nsw.gov.au>
2025-07-31 15:47:16 +0000

    Not feeling so good. Just find out that my progress an commits have not been saving and my code space had become corrupted

--- char.test.py
+++ char.test.py
@@ -2,5 +2,9 @@ from character import Enemy
 
 harry = Enemy("Harry", "A dirty, smelly Wumpus")
 harry.describe()
-harry.set_conversation("Come closer I cannot see you little little one.")
+harry.set_conversation("Come closer I cannot see you little one.")
 harry.talk()
+harry.set_weakness("vegemite")
+
+fight_with = input("What do you want to fight with?")
+harry.fight(fight_with)
